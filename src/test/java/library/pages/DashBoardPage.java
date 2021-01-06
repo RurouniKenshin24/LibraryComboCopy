d5e0465ec5240c71071d88fd4da7ba07c48c77e4
RurouniKenshin24 <ogzhn_9225@hotmail.com>
2021-01-05 23:45:45 -0600

    Initial Commit

---
+++ src/test/java/library/pages/DashBoardPage.java
@@ -0,0 +1,29 @@
+package library.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class DashBoardPage extends BasePage {
+    @FindBy(id = "user_count")
+    public WebElement userCount;
+
+    @FindBy(id = "book_count")
+    public WebElement bookCount;
+
+    @FindBy(id = "borrowed_books")
+    public WebElement borrowedBooksCount;
+
+    public void click_On(String link) {
+        switch (link.toLowerCase()) {
+            case "dashboard":
+                dashboard.click();
+                break;
+            case "users":
+                users.click();
+                break;
+            case "books":
+                books.click();
+                break;
+        }
+    }
+}
